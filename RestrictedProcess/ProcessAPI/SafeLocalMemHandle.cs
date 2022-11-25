38497e8a2d91074b485a891b197578eb0134b010
andyhebear <andyhebear@163.com>
2022-11-25 16:43:17 +0800

    Create SafeLocalMemHandle.cs

---
+++ RestrictedProcess/ProcessAPI/SafeLocalMemHandle.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="SafeLocalMemHandle.cs" company="Nikolay Kostov (Nikolay.IT)">
+// Copyright (c) Nikolay Kostov (Nikolay.IT). All Rights Reserved.
+// Licensed under the Apache License. See LICENSE in the project root for license information.
+// </copyright>
+
+namespace Sandbox_Process.RestrictedProcess
+{
+    using System;
+    using System.Security;
+    using System.Security.Permissions;
+
+    using Microsoft.Win32.SafeHandles;
+
+    [SuppressUnmanagedCodeSecurity]
+    [HostProtection(SecurityAction.LinkDemand, MayLeakOnAbort = true)]
+    internal sealed class SafeLocalMemHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        internal SafeLocalMemHandle()
+            : base(true)
+        {
+        }
+
+        [SecurityPermission(SecurityAction.LinkDemand, UnmanagedCode = true)]
+        internal SafeLocalMemHandle(IntPtr existingHandle, bool ownsHandle)
+            : base(ownsHandle)
+        {
+            this.SetHandle(existingHandle);
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            return NativeMethods.LocalFree(this.handle) == IntPtr.Zero;
+        }
+    }
+}
